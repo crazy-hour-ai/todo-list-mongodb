fb08cd9266dbbd706181ad2080d4ce89b1b26682
lim <e0374012@u.nus.edu>
2020-02-29 23:12:37 +0800

    Add Mongoose connection and create Todo Model

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const app = express();
 
 const PORT = 3000;
 
+const mongoose = require('mongoose')                    // 載入 mongoose
+mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true, useUnifiedTopology: true })   // 設定連線到 mongoDB
+
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection;
+
+const Todo = require('./models/todo');
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error');
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected');
+})
+
+
 app.get('/', (req, res) => {
   res.send('hello world');
 })
---
+++ models/todo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const todoSchema = new schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema);

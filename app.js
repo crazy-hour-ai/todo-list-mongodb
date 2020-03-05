0f286a49e2500507d2cc529e167984ce1590bff9
lim <e0374012@u.nus.edu>
2020-03-05 22:57:04 +0800

    Add method-override and update to RESTful routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+// 引用 method-override
+const methodOverride = require('method-override');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +10,8 @@ const PORT = 3000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
+// 設定 method-override
+app.use(methodOverride('_method'));
 
 const mongoose = require('mongoose')                    // 載入 mongoose
 mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true, useUnifiedTopology: true })   // 設定連線到 mongoDB
@@ -98,7 +102,7 @@ app.get('/todos/:id/edit', (req, res) => {
     })
 })
 // 修改 Todo
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id/edit', (req, res) => {
   // res.send('修改 Todo')
   Todo.findById(req.params.id, (err, todoEditById) => {
     if (err)
@@ -125,7 +129,7 @@ app.post('/todos/:id/edit', (req, res) => {
 
 
 // 刪除 Todo
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id/delete', (req, res) => {
   // res.send('刪除 Todo')
   Todo.findById(req.params.id, (err, todoDelete) => {
     if (err)

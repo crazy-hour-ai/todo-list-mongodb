4699deeed67ad6d86050d3d20eef5f40c3120527
lim <e0374012@u.nus.edu>
2020-03-07 16:32:31 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,35 @@ router.get('/register', (req, res) => {
 
 //register check
 router.post('/register', (req, res) => {
-  res.send('register');
+
+  const { name, email, password, password2 } = req.body;
+
+  User.findOne({ email: email })
+    .then(user => {
+      if (user) {
+        console.log('User already exits')
+        res.render('register', {
+          name,
+          email,
+          password,
+          password2
+        })
+      }
+      else {
+        const newUser = new User({ // 如果 email 不存在就直接新增
+          name,
+          email,
+          password
+        })
+        newUser
+          .save()
+          .then(user => {
+            res.redirect('/') // 新增完成導回首頁
+          })
+          .catch(err => console.log(err))
+      }
+    })
+  // res.send('register');
 })
 
 //logout

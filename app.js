7c8e115a6a9b2c1d5012472ff5d22868d7b458a5
lim <e0374012@u.nus.edu>
2020-03-07 22:00:44 +0800

    Add user login feature using passport

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const session = require('express-session');
+const passport = require('passport');
 
 const mongoose = require('mongoose')                    // 載入 mongoose
 mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true, useUnifiedTopology: true })   // 設定連線到 mongoDB
@@ -21,6 +22,7 @@ mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true, useU
 // mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
 const db = mongoose.connection;
 const Todo = require('./models/todo');
+
 
 // 連線異常
 db.on('error', () => {
@@ -32,15 +34,35 @@ db.once('open', () => {
   console.log('mongodb connected');
 })
 
-app.use('/', require('./routes/home'));
-app.use('/todos', require('./routes/todo'));
-app.use('/users', require('./routes/user'));
 
+// 使用 Session --------------------------------
 app.use(session({
   secret: "your secret key",
   resave: false,
   saveUninitialized: true
 }))
+
+
+// 使用 Passport -------------------------------
+app.use(passport.initialize())
+app.use(passport.session())
+
+// 載入 Passport config
+require('./config/passport')(passport)
+
+// 登入後可以取得使用者的資訊方便我們在 view 裡面直接使用
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
+
+
+
+//router middleware--------------------------------
+app.use('/', require('./routes/home'));
+app.use('/todos', require('./routes/todo'));
+app.use('/users', require('./routes/user'));
+
 
 app.listen(PORT, () => {
   console.log(`App is running at http://localhost:${PORT}`)
--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,19 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+const passport = require('passport');
+
 //login page
 router.get('/login', (req, res) => {
   res.render('login');
 })
 
 //login check
-router.post('/login', (req, res) => {
-  res.send('login');
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', { // 使用 passport 認證
+    successRedirect: '/', // 登入成功會回到根目錄
+    failureRedirect: '/users/login' // 失敗會留在登入頁面
+  })(req, res, next)
 })
 
 //register page
